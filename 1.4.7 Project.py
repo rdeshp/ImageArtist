c5a3b7462b803142060abc8554917a35bd2b9865
rdeshp <33662388+rdeshp@users.noreply.github.com>
2017-12-12 10:17:33 -0500

    Images now as PIL. Image Save to output folder

--- 1.4.7 Project.py
+++ 1.4.7 Project.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os.path
 import numpy as np  # "as" lets us use standard abbreviations
+import PIL
    
 '''Read the image data'''
 # Get the directory of this python script
@@ -8,7 +9,7 @@ directory = os.path.dirname(os.path.abspath(__file__))
 # Build an absolute filename from directory + filename
 filename = os.path.join(directory, '1.4.5 Images\\car.jpg')
 # Read the image data into an array
-img = plt.imread(filename)
+img = PIL.Image.open(filename)
   
 '''Show the image data'''
 # Create figure with 1 subplot
@@ -36,6 +37,15 @@ def blackWhiteFilter():
     # Show the figure on the screen
     fig.show()
     
+    
+def frame(thickness = 10):
+    height = len(img)
+    width = len(img[0])    
+    
+    image_sheet = Image.new("RGBA", (max_width * len(images), max_height))
+    
+    
+    
 def reloadImage():
     directory = os.path.dirname(os.path.abspath(__file__)) 
     # Build an absolute filename from directory + filename
@@ -49,4 +59,10 @@ def reloadImage():
     
     # Show the figure on the screen
     fig.show()
+    
+def saveImg(imgName):
+    directory = os.path.dirname(os.path.abspath(__file__)) 
+    # Build an absolute filename from directory + filename
+    filename = os.path.join(directory, 'Output Images\\'+imgName+'.png')
+    img.save(filename, "PNG")
     
