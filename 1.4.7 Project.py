ef38a760186bb4fc5543b5151f1ad4df48d2636c
rdeshp <33662388+rdeshp@users.noreply.github.com>
2017-12-12 10:03:04 -0500

    Added Black and White Filter... and reload Image function

--- 1.4.7 Project.py
+++ 1.4.7 Project.py
@@ -14,11 +14,39 @@ img = plt.imread(filename)
 # Create figure with 1 subplot
 fig, ax = plt.subplots(1, 1)
 
-height = len(img)
-width = len(img[0])
+
 
 # Show the image data in a subplot
 ax.imshow(img, interpolation='none')
 
 # Show the figure on the screen
 fig.show()
+
+
+def blackWhiteFilter():
+    height = len(img)
+    width = len(img[0])
+    for i in range(height):
+        for j in range(width):
+            val = sum(img[i][j])
+            img[i][j][0] = val
+            img[i][j][1] = val
+            img[i][j][2] = val
+    ax.imshow(img, interpolation='none')
+    # Show the figure on the screen
+    fig.show()
+    
+def reloadImage():
+    directory = os.path.dirname(os.path.abspath(__file__)) 
+    # Build an absolute filename from directory + filename
+    filename = os.path.join(directory, '1.4.5 Images\\car.jpg')
+    # Read the image data into an array
+    img = plt.imread(filename)
+        
+    
+    # Show the image data in a subplot
+    ax.imshow(img, interpolation='none')
+    
+    # Show the figure on the screen
+    fig.show()
+    
