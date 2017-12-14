1819b10ebe8529110798d2283843877602aab33c
rdeshp <33662388+rdeshp@users.noreply.github.com>
2017-12-14 10:18:23 -0500

    Add Color and Frame function added

--- 1.4.7 Project.py
+++ 1.4.7 Project.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import os.path
 import numpy as np  # "as" lets us use standard abbreviations
 import PIL
+from matplotlib.widgets import Slider, Button, RadioButtons
    
 '''Read the image data'''
 # Get the directory of this python script
@@ -10,7 +11,7 @@ directory = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(directory, '1.4.5 Images\\flower2.jpg')
 # Read the image data into an array
 img = PIL.Image.open(filename)
-  
+
 '''Show the image data'''
 # Create figure with 1 subplot
 fig, ax = plt.subplots(1, 1)
@@ -90,12 +91,45 @@ def keyFilter(red, green, blue, thresh):
     fig.show()
 
 
+def addColor(red, green, blue):
+    width, height = img.size
+    pixels = img.load()
+    for i in range(height):
+        for j in range(width):
+            r, g, b = img.getpixel((j, i))
+            newRed = r + red
+            newGreen = g + green
+            newBlue = b + blue
+            
+            if newRed > 255:
+                newRed = 255
+            if newGreen > 255:
+                newGreen = 255
+            if newBlue > 255:
+                newBlue = 255
+            if img.mode == 'RGB':
+                shade = (newRed, newGreen, newBlue)
+            elif img.mode == 'RGBA':
+                shade = (newRed, newGreen, newBlue, 255)
+            pixels[j,i]= shade
+    ax.imshow(img, interpolation='none')
+    # Show the figure on the screen
+    fig.show()
 
-def frame(thickness = 10):
-    height = len(img)
-    width = len(img[0])    
-    
-    image_sheet = Image.new("RGBA", (max_width * len(images), max_height))
+
+
+def frame(thickness = 20, color = (0,0,0)):
+    width, height = img.size  
+    image_sheet = PIL.Image.new("RGB", (width+2*thickness, height+2*thickness), color)
+    newPixels = image_sheet.load()
+    for i in range(height):
+        for j in range(width):
+            r, g, b = img.getpixel((j, i))
+            shade = (r, g, b)
+            newPixels[j+thickness,i+thickness]= shade
+    ax.imshow(image_sheet, interpolation='none')
+    # Show the figure on the screen
+    fig.show()
     
     
     
